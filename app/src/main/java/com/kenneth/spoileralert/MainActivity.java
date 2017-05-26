5dcb9e9e27e7a7959dbcf2c50dfd1ca8c3a2b604
Kenneth Gitere <gitere81@gmail.com>
2017-05-26 13:12:07 +0300

    added Toast and made new activity

--- app/src/main/java/com/kenneth/spoileralert/MainActivity.java
+++ app/src/main/java/com/kenneth/spoileralert/MainActivity.java
@@ -2,9 +2,15 @@ package com.kenneth.spoileralert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.categoriesListView) ListView mListView;
@@ -15,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        CategoriesArrayAdapter adapter = new CategoriesArrayAdapter(this,android.R.layout.simple_list_item_1,categories);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView,View view,int i,long l){
+                String categoryname = ((TextView)view).getText().toString();
+                Toast.makeText(MainActivity.this,"Viewing "+categoryname,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
