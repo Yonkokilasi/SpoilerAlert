f6c620bbcb1d61964ce662d8053a446b2c63a14e
Kenneth Gitere <gitere81@gmail.com>
2017-06-01 09:32:40 +0300

    add OkHttp and SignPost dependencies

--- app/src/main/java/com/kenneth/spoileralert/Constants.java
+++ app/src/main/java/com/kenneth/spoileralert/Constants.java
@@ -5,4 +5,8 @@ package com.kenneth.spoileralert;
  */
 
 public class Constants {
+    public static final String CONSUMER_KEY = BuildConfig.TWITTER_CONSUMER_KEY;
+    public static final String CONSUMER_SECRET = BuildConfig.TWITTER_CONSUMER_SECRET;
+    public static final String TOKEN = BuildConfig.TWITTER_TOKEN;
+    public static final String TOKEN_SECRET = BuildConfig.TWITTER_TOKEN_SECRET;
 }
