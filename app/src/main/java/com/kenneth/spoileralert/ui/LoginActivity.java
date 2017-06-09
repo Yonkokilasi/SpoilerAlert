a93979df529fcac2a40ef0a1070d03fe4e224c24
Kenneth Gitere <gitere81@gmail.com>
2017-06-09 13:07:08 +0300

    add AuthStateListener in LoginActivity

--- app/src/main/java/com/kenneth/spoileralert/ui/LoginActivity.java
+++ app/src/main/java/com/kenneth/spoileralert/ui/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.kenneth.spoileralert.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +10,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.kenneth.spoileralert.R;
 
 import butterknife.Bind;
@@ -22,6 +27,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Bind(R.id.newUserTextView) TextView mNewUserTextView;
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +36,36 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         ButterKnife.bind(this);
 
         mAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user!=null){
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
 
         mLoginButton.setOnClickListener(this);
         mNewUserTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if (mAuthStateListener!=null){
+            mAuth.removeAuthStateListener(mAuthStateListener);
+        }
     }
 
     @Override
@@ -56,5 +89,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         if (password.equals("")){
             mPasswordEditText.setError("Password field is empty. Please fill it in");
         }
+
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Signed in!", Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(LoginActivity.this, "Sign in failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
