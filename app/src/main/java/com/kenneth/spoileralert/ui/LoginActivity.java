fd5ac32a905f259647600f72a218ad5a7f4e9aff
Kenneth Gitere <gitere81@gmail.com>
2017-06-09 12:58:05 +0300

    add AuthStateListeners and Login function

--- app/src/main/java/com/kenneth/spoileralert/ui/LoginActivity.java
+++ app/src/main/java/com/kenneth/spoileralert/ui/LoginActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.kenneth.spoileralert.R;
 
 import butterknife.Bind;
@@ -20,11 +21,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Bind(R.id.loginButton) Button mLoginButton;
     @Bind(R.id.newUserTextView) TextView mNewUserTextView;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
 
         mLoginButton.setOnClickListener(this);
         mNewUserTextView.setOnClickListener(this);
@@ -33,11 +38,23 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v){
         if (v==mLoginButton){
-            Toast.makeText(this, "Login Coming Soon!", Toast.LENGTH_SHORT).show();
+           existingUserLogin();
         }
         if (v==mNewUserTextView){
             Intent intent = new Intent(LoginActivity.this,NewUserActivity.class);
             startActivity(intent);
+        }
+    }
+
+    private void existingUserLogin(){
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        if (email.equals("")){
+            mEmailEditText.setError("Please enter a valid email address");
+            return;
+        }
+        if (password.equals("")){
+            mPasswordEditText.setError("Password field is empty. Please fill it in");
         }
     }
 }
