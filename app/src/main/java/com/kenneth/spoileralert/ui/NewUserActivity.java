6fc7ae3e5dc4dfbee42fee3c63c7302c4fee759d
Kenneth Gitere <gitere81@gmail.com>
2017-06-09 12:08:35 +0300

    add Intent from NewUserActivity to LoginActivity

--- app/src/main/java/com/kenneth/spoileralert/ui/NewUserActivity.java
+++ app/src/main/java/com/kenneth/spoileralert/ui/NewUserActivity.java
@@ -1,15 +1,44 @@
 package com.kenneth.spoileralert.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kenneth.spoileralert.R;
 
-public class NewUserActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class NewUserActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @Bind(R.id.newUserSubmitButton) Button mNewUserSubmitButton;
+    @Bind(R.id.userLoginTextView) TextView mUserLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+        ButterKnife.bind(this);
+
+        mNewUserSubmitButton.setOnClickListener(this);
+        mUserLoginTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if (v==mNewUserSubmitButton){
+            Toast.makeText(this, "Submit User Here", Toast.LENGTH_SHORT).show();
+        }
+        if (v==mUserLoginTextView){
+            Intent intent = new Intent(NewUserActivity.this,LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
