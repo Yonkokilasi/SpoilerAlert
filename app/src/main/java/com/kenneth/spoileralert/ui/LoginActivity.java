4803c64509563f53cb4afef975cb492a2d62cacb
Kenneth Gitere <gitere81@gmail.com>
2017-06-09 11:59:52 +0300

    add Intent from LoginActivity to NewUserActivity and add UI

--- app/src/main/java/com/kenneth/spoileralert/ui/LoginActivity.java
+++ app/src/main/java/com/kenneth/spoileralert/ui/LoginActivity.java
@@ -1,15 +1,43 @@
 package com.kenneth.spoileralert.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kenneth.spoileralert.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.loginButton) Button mLoginButton;
+    @Bind(R.id.newUserTextView) TextView mNewUserTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mLoginButton.setOnClickListener(this);
+        mNewUserTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if (v==mLoginButton){
+            Toast.makeText(this, "Login Coming Soon!", Toast.LENGTH_SHORT).show();
+        }
+        if (v==mNewUserTextView){
+            Intent intent = new Intent(LoginActivity.this,NewUserActivity.class);
+            startActivity(intent);
+        }
     }
 }
